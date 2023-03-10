df2eefa9abd46692288d4050fa9ecc90b1df5ac0
Khemmathat Phoowayarnapong <khemmathat.p@firecreekweb.com>
2023-03-10 23:46:38 +0700

    refactor: update entity

--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -5,4 +5,6 @@ public class User
     public string Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Ip { get; set; }
 }
--- Core/IUserRepository.cs
+++ Core/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace Core;
 public interface IUserRepository
 {
     public Task Create(User user);
+    public Task Update();
     public Task<IEnumerable<User>> GetUsers();
     public Task<User> GetUser();
-    public Task Update();
 }
